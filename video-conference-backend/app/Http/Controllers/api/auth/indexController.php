959eb795952fc43d62885351624e3e35b1978be8
burak zafer ozcan <ozcanburakzafer@gmail.com>
2024-09-29 21:35:10 +0300

    feature: LoginRequest created.

---
+++ video-conference-backend/app/Http/Controllers/api/auth/indexController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\api\auth;
+
+use App\Http\Controllers\api\BaseController;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class indexController extends BaseController
+{
+
+}
