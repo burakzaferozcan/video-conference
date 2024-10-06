95df78a05f05ea8fc944a5bbae60ee234010e757
burak zafer ozcan <ozcanburakzafer@gmail.com>
2024-10-06 16:05:39 +0300

    feature: video conferencing process completed

--- video-conference-frontend/src/pages/Video/index.jsx
+++ video-conference-frontend/src/pages/Video/index.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { Component, createRef } from "react";
 import Header from "../../Components/Common/Header";
 import { inject, observer } from "mobx-react";
 import withRouter from "../../withRouter";
@@ -7,15 +7,79 @@ import { Card, Col, Container, Row } from "react-bootstrap";
 import RestClient from "../../RestAPI/RestClient";
 import AppUrl from "../../RestAPI/AppUrl";
 import Notification from "../../RestAPI/Notification";
+import { Peer } from "peerjs";
+
+const HOST = "127.0.0.1";
+const PORT = 4444;
 
 class Video extends Component {
   constructor(props) {
     super(props);
+
+    this.localVideoRef = createRef();
+    this.remoteVideoRef = createRef();
   }
 
   componentDidMount() {
     this.checkUser();
+    this.startVideoChat();
   }
+
+  componentWillUnmount() {
+    this.peer.destroy();
+  }
+
+  startVideoChat = () => {
+    const { params } = this.props;
+    this.peer = new Peer(this.props.AuthStore.appState.user.conn_string, {
+      host: HOST,
+      port: PORT,
+      key: "webrtc",
+      path: "/",
+    });
+
+    var getUserMedia =
+      navigator.getUserMedia ||
+      navigator.webkitGetUserMedia ||
+      navigator.mozGetUserMedia;
+    const _self = this;
+
+    getUserMedia(
+      { video: true, audio: true },
+      function (stream) {
+        if (_self.localVideoRef.current) {
+          _self.localVideoRef.current.srcObject = stream;
+        }
+
+        const call = _self.peer.call(params.conn, stream);
+        call.on("stream", function (remoteStream) {
+          if (_self.remoteVideoRef.current) {
+            _self.remoteVideoRef.current.srcObject = remoteStream;
+          }
+        });
+      },
+      function (err) {
+        console.log("Stream error: ", err);
+      }
+    );
+
+    _self.peer.on("call", function (call) {
+      getUserMedia(
+        { video: true, audio: true },
+        function (stream) {
+          call.answer(stream);
+          call.on("stream", function (remoteStream) {
+            if (_self.remoteVideoRef.current) {
+              _self.remoteVideoRef.current.srcObject = remoteStream;
+            }
+          });
+        },
+        function (err) {
+          console.log("Stream error: ", err);
+        }
+      );
+    });
+  };
 
   checkUser = () => {
     const { navigate, params } = this.props;
@@ -65,7 +129,6 @@ class Video extends Component {
       <>
         <AuthLayout>
           <Header />
-
           <Container>
             <Row className={"mt-5"}>
               <Col md={12}>
@@ -79,10 +142,22 @@ class Video extends Component {
                   </Card.Header>
                   <Card.Body>
                     <p>Bizim Kameramız</p>
-                    <video playsInline autoPlay width={300} height={300} />
+                    <video
+                      ref={this.localVideoRef}
+                      playsInline
+                      autoPlay
+                      width={300}
+                      height={300}
+                    />
                     <hr />
                     <p>Karşı Tarafın Kamerası</p>
-                    <video playsInline autoPlay width={300} height={300} />
+                    <video
+                      ref={this.remoteVideoRef}
+                      playsInline
+                      autoPlay
+                      width={300}
+                      height={300}
+                    />
                   </Card.Body>
                 </Card>
               </Col>
